184b64b95dc28b4af1ec3e402fa37d8ba615f7d8
Haris Pasic <harispasic@ymail.com>
2022-04-13 02:28:26 +0200

    Sharing cb using export

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+exports.firstName = (firstName) => {
+    return firstName.toUpperCase();
+}
+
+exports.lastName = (lastName) => {
+return lastName.toLowerCase();
+}
+
+console.log(this.firstName('haris'));
+console.log(this.lastName('PASIC'));
+
+
+
